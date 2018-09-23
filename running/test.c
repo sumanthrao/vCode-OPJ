e9c9835cd22e964f488843cec92eb4701388a85f
sumanthrao <sraosumanth@gmail.com>
2018-09-23 13:16:24 +0530

    updates

--- running/test.c
+++ running/test.c
@@ -1,17 +1,16 @@
 #include "../setlimits.c"
 #include<stdio.h>
 #include<stdlib.h>
-
 int main(int argc, char* argv[]){
 	setlimits(argc,argv);
   // to test memory -  defult limit is 1024 bytes
-	int* arr = malloc(sizeof(int)*1000);
+	//int* arr = malloc(sizeof(int)*1000);
   //timeouot limit - 5 seconds
-	for(int i=0;i<1000000;i++){
-		for(int j=0;j<100000;j++){
-			;
-		}
-	}
+	// for(int i=0;i<1000000;i++){
+	// 	for(int j=0;j<100000;j++){
+	// 		;
+	// 	}
+	// }
 	//Try opening more than one file 
   
    FILE *fp = NULL; 
